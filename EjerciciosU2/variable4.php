fea20180477bce957efbaf2c44f71eae9ff170dc
nestorsvc <nestorserna100@gmail.com>
2025-09-24 20:13:33 +0200

    primeros ejercicios

---
+++ EjerciciosU2/variable4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 5</title>
+</head>
+<body>
+    <?php
+    $boleano = true;
+    $entero = 1;
+    $flotante = 1.1;
+    $cadena = 'hola';
+    echo("El boleano es: $boleano <br>El entero es $entero<br>El flotante es $flotante<br>La cadena es $cadena");
+    ?>
+</body>
+</html>
